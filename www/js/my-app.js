5818f7e291dc1efd77684560d66b45b0a83da398
kitiievans <magali.benitez17@gmail.com>
2021-06-10 18:01:54 -0300

    index no vuelve

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -17,7 +17,7 @@ var app = new Framework7({
     routes: [
       {
         path: '/agenda/',
-        url: 'about.html',
+        url: 'agenda.html',
       },
       {
         path: '/index/',
@@ -41,37 +41,80 @@ var mainView = app.views.create('.view-main');
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
-
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
     console.log(e);
     
 })
-
-// Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
-})
-
+//Página de inicio
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
   $$('#login').on('click', function () {
-    console.log("click sí")
-    mainView.router.navigate('/login-screen/')
+    console.log("click sí");
+    mainView.router.navigate('/login-screen/');
   });
+  $$('#registro').on('click', function () {
+    console.log("click sí");
+    mainView.router.navigate('/registro/');
+  });
+  $$('#agenda').on('click', function () {
+    console.log("click sí");
+    mainView.router.navigate('/agenda/');
+  });
+})
+  //Página de inicio de sesión 
+$$(document).on('page:init', '.page[data-name="login-screen"]', function (e) {
+  $$('#volver').on('click', function () {
+    console.log("click sí");
+    mainView.router.navigate('/index/');
+  }) 
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+    $$("#iniciasesion").on('click', function () {
+      mainView.router.navigate('/agenda/')
+      console.log("tu vieja se logueó")
+    })
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  })
+  
 
 
+//Página de registro
+  $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
   var email = "usuario@dominio.com";
   var clave = "1234Abc";
-/*
   firebase.auth().createUserWithEmailAndPassword(email, clave)
-  .then(function(){
-    console.log("que paso?")
+  .then((userCredential) => {
+    // Signed in
+    console.log("tu vieja se registró")
+    var user = userCredential.user;
+    var email= $$("#email")
+    // ...
+    
   })
   .catch(function(error){ //este error es un json
-    console.error(error.code)
-    console.error(error.mensaje)
-  });*/
+    console.error(error.code);
+    if (error.code == "auth/email-already-in-use"){
+      console.error("El mail ya existe");
+    }
+    console.error(error.message)
+  });
+  })
+  $$(document).on('click', function () {
+    $$('#iniciarsesionR').on('click', '/index/')
+  })
 })
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="agenda"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+  alert('Hello');
+})
+  
